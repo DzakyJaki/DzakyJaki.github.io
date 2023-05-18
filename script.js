0d21516af49d5ec7b084bdb9ccd9ba53476d52df
DzakyJaki <efendidzakyy@gmail.com>
2023-05-18 22:05:35 +0700

    nambahin scroll animation

---
+++ script.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 1500,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.about',{delay:200, origin: 'top'});
+sr.reveal('.project',{delay:200, origin: 'top'});
+sr.reveal('.contact',{delay:200, origin: 'top'});
+sr.reveal('.border-5',{delay:200, origin: 'top'});
+sr.reveal('.display-4',{delay:250, origin: 'left'});
+sr.reveal('.lead',{delay:300, origin: 'bottom'});
+
