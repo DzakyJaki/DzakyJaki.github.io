e552bf711e7deea002c7aa48fde3df358d3f170a
DzakyJaki <efendidzakyy@gmail.com>
2023-05-20 13:53:18 +0700

    benerin scroll reveal

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-const sr = ScrollReveal ({
-    distance: '65px',
-    duration: 1500,
-    delay: 450,
-    reset: true
+const sr = ScrollReveal({
+  distance: "65px",
+  duration: 900,
+  reset: true,
 });
 
-sr.reveal('.about',{delay:200, origin: 'top'});
-sr.reveal('.project',{delay:200, origin: 'top'});
-sr.reveal('.contact',{delay:200, origin: 'top'});
-sr.reveal('.border-5',{delay:200, origin: 'top'});
-sr.reveal('.display-4',{delay:250, origin: 'left'});
-sr.reveal('.lead',{delay:300, origin: 'bottom'});
+// navbar scroll animation
+sr.reveal(".navbar-brand", { delay: 200, origin: "left", reset: false });
+sr.reveal(".navbar-nav", { delay: 200, origin: "right", reset: false });
 
+// section scroll animation
+sr.reveal(".about", { delay: 200, origin: "top" });
+sr.reveal(".project", { delay: 200, origin: "top" });
+sr.reveal(".contact", { delay: 200, origin: "top" });
+
+// jumbotron scroll animation
+sr.reveal(".img", { delay: 100, origin: "top" });
+sr.reveal(".name", { delay: 200, origin: "top" });
+sr.reveal(".profession", { delay: 300, origin: "top" });
+
+// project scroll animation
+sr.reveal(".card-yt", { delay: 200, origin: "top", reset: true });
+sr.reveal(".card-ig", { delay: 300, origin: "top", reset: true });
+sr.reveal(".card-netflix", { delay: 400, origin: "top", reset: true });
+sr.reveal(".card-tiktok", { delay: 200, origin: "top", reset: true });
+sr.reveal(".card-twitter", { delay: 300, origin: "top", reset: true });
+
+// contact scroll animation
+sr.reveal(".contact-identity", { delay: 200, origin: "top", reset: true });
+sr.reveal(".contact-form", { delay: 300, origin: "top", reset: true });
